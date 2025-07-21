d82ab80554578d68f5073eccd010b3293ad99213
Jyothi Bhavani Ravipati <bhavani.r1660@gmail.com>
2025-07-21 23:09:30 +0530

    Update full_adder.v

--- Adders/full_adder/full_adder.v
+++ Adders/full_adder/full_adder.v
@@ -1,1 +1,9 @@
-
+module full_adder(input a,b,c,
+                  output sum,carry);
+  wire w1,w2,w3;
+  assign sum = a^b^c;
+  assign w1 = a & b;
+  assign w2 = b & c;
+  assign w3 = a & c;
+  assign carry = w1|w2|w3;
+endmodule
