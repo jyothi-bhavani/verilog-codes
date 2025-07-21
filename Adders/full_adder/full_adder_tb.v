fc0d4c1541f6dfc5f0ddf2d2fa1d5794ae903075
Jyothi Bhavani Ravipati <bhavani.r1660@gmail.com>
2025-07-21 23:19:43 +0530

    Update full_adder_tb.v

--- Adders/full_adder/full_adder_tb.v
+++ Adders/full_adder/full_adder_tb.v
@@ -1,1 +1,25 @@
-
+module full_adder_tb();
+  reg a,b,c;
+  reg w1,w2,w3;
+  wire sum,carry;
+  
+  full_adder dut(a,b,c,sum,carry);
+  
+  initial 
+    begin
+      a=0; b=0; c=0; #10
+      a=0; b=0; c=1; #10
+      a=0; b=1; c=0; #10
+      a=0; b=1; c=1; #10
+      a=1; b=0; c=0; #10
+      a=1; b=0; c=1; #10
+      a=1; b=1; c=0; #10
+      a=1; b=1; c=1; #10
+      $finish;
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+endmodule
+      
