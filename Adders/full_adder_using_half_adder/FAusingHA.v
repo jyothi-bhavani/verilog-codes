875d0a889fa634c335ae11a8397f85e7eae02831
Jyothi Bhavani Ravipati <bhavani.r1660@gmail.com>
2025-07-22 22:42:47 +0530

    Update FAusingHA.v

--- Adders/full_adder_using_half_adder/FAusingHA.v
+++ Adders/full_adder_using_half_adder/FAusingHA.v
@@ -1,1 +1,11 @@
+module FA_HA(input a, b, cin,
+             output sum, carry);
+  wire s1, c1, c2;
 
+  assign s1 = a ^ b;
+  assign c1 = a & b;
+  assign sum = s1 ^ cin;
+  assign c2 = s1 & cin;
+  assign carry = c1 | c2;
+
+endmodule
