c6eed353f839cba628369d0f0fc87217dff50f81
Jyothi Bhavani Ravipati <bhavani.r1660@gmail.com>
2025-07-22 22:43:49 +0530

    Update FAusingHA_tb.v

--- Adders/full_adder_using_half_adder/FAusingHA_tb.v
+++ Adders/full_adder_using_half_adder/FAusingHA_tb.v
@@ -1,1 +1,17 @@
-
+module FA_HA_tb();
+  reg a, b, c;
+  wire sum, carry;
+  FA_HA dut(a, b, c, sum, carry);
+  initial begin    
+    for (integer count = 0; count < 8; count = count + 1) begin
+      {a, b, c} = count[2:0]; 
+      #10;
+    end
+    $finish;
+  end
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+    end
+endmodule                                                                     
