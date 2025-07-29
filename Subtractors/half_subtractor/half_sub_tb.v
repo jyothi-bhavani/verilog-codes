b74978f3627a74d7fe178ecb6d6c6b522d075e70
Jyothi Bhavani Ravipati <bhavani.r1660@gmail.com>
2025-07-29 22:23:29 +0530

    Update half_sub_tb.v

--- Subtractors/half_subtractor/half_sub_tb.v
+++ Subtractors/half_subtractor/half_sub_tb.v
@@ -1,1 +1,18 @@
-
+module half_sub_tb();
+reg a,b;
+wire sum,barrow;
+  half_sub dut(.a(a),.b(b),.sum(sum),.borrow(borrow));
+initial
+begin
+   $dumpfile("dump.vcd");
+   $dumpvars;
+  a=0;b=0;
+  #20;
+  a=0;b=1;
+  #20;
+  a=1;b=0;
+  #20;
+  a=1;b=1;
+  $finish();
+end
+endmodule
